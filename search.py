b3bf4f3024ae298271ea992c9cfb88fd43728b18
lukmanmemon <memon_lukman@hotmail.com>
2021-11-01 15:18:38 -0400

    Added query vector/length and cosine similarity scores

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import ast
 import math
 import numpy as np
+from operator import itemgetter
 import nltk
 nltk.download('punkt')
 
@@ -25,6 +26,14 @@ query_tokens = nltk.word_tokenize(query_input)
 # Filter list to keep terms that are in dictionary
 query_terms = [word for word in query_tokens if word in dictionary]
 sorted_query_terms = sorted(query_terms)
+
+# Get query term frequencies
+query_terms_frequencies = {}
+for word in sorted_query_terms:
+    if word in query_terms_frequencies:
+        query_terms_frequencies[word] = query_terms_frequencies[word] + 1
+    else:
+        query_terms_frequencies[word] = 1
 
 # Add idf of query terms to dictionary
 terms_idf = {}
@@ -58,3 +67,27 @@ for id, vector in document_vectors_dict.items():
         
     length = math.sqrt(sum)
     document_vector_lengths[id] = length
+
+# Get query vector
+query_vector = np.zeros((len(query_terms)), dtype=float)
+q_index = 0
+for term in sorted_query_terms:
+     # Get weight by idf * tf
+    query_vector[q_index] = terms_idf[term] * query_terms_frequencies[term]
+    q_index = q_index + 1
+
+# Calculate query vector length
+qvector_sum = 0
+for weight in query_vector:
+    qvector_sum = qvector_sum + weight ** 2
+
+qvector_length = math.sqrt(qvector_sum)
+
+# Calculate cosine similarity
+cosine_similarity = {}
+
+for id in range(1, 3205):
+    similarity_score = 0
+    similarity_score = (document_vectors_dict[id].dot(query_vector)) / (document_vector_lengths[id] * qvector_length)
+    cosine_similarity[id] = similarity_score
+
